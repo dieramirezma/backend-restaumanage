ba4d76bccf1f4233a09d88715221f5bed670ffe1
dieramirezma <dieramirezma@unal.edu.co>
2024-07-19 18:17:52 -0500

    refactor: Update database connection to use production MongoDB URI when in production environment

--- database/connection.js
+++ database/connection.js
@@ -2,8 +2,10 @@ import { connect } from 'mongoose'
 
 const connection = async () => {
   try {
-    const DEV_MONGODB_URI = process.env.DEV_MONGODB_URI
-    await connect(DEV_MONGODB_URI)
+    const MONGODB_URI = process.env.NODE_ENV === 'production'
+      ? process.env.PROD_MONGODB_URI
+      : process.env.DEV_MONGODB_URI
+    await connect(MONGODB_URI)
     console.log('Connected to the database in Atlas')
   } catch (error) {
     console.log(error)
