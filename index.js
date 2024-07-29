90d8da13ea2540f0e6bc0b329a7ce926dc0fad7f
dieramirezma <dieramirezma@unal.edu.co>
2024-07-28 21:54:43 -0500

    update: Allow all origins

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: '*',
   methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Content-Type, Authorization',
   optionsSuccessStatus: 200,
